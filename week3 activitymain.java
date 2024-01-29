89a18b1da1fd9fc6f9e199107b52273af409060b
SONA4414 <113096956+SONA4414@users.noreply.github.com>
2024-01-29 22:28:55 +0530

    week3 activitymain.java

---
+++ week3 activitymain.java
@@ -0,0 +1,29 @@
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+Button btnToast;
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main);
+btnToast=findViewById(R.id.btnToast);
+btnToast.setOnClickListener(new View.OnClickListener() {
+public void onClick(View v) {
+Toast toast = new Toast(getApplicationContext());
+View view = getLayoutInflater().inflate(R.layout.custom_toast_layout, (ViewGroup)
+findViewById((R.id.viewContainer)));
+toast.setView(view);
+TextView txtMsg=view.findViewById(R.id.txtMsg);
+txtMsg.setText("Message Sent Sussecufully");
+toast.setDuration(Toast.LENGTH_LONG);
+toast.show();
+toast.setGravity(Gravity.TOP | Gravity.END, 0,0);
+}
+}); }
+}
